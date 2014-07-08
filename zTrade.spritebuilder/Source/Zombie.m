ba7e06250b432f0d7369e53c23df0051ecf0498d
Jordan Arnesen <jordan101112@gmail.com>
2014-07-07 18:50:15 -0700

    zombie moving through action sequence

--- zTrade.spritebuilder/Source/Zombie.m
+++ zTrade.spritebuilder/Source/Zombie.m
@@ -8,17 +8,42 @@
 
 #import "Zombie.h"
 
-@implementation Zombie {
+@implementation Zombie
+{
     CCActionSequence *currentPath;
     NSMutableArray *pathPoints;
+    NSMutableArray *testPath;
     CGPoint touchLocation;
     CGPoint targetPoint;
+    
+    CGPoint one;
+    CGPoint two;
+    CGPoint three;
+    CGPoint four;
+    
+    BOOL isFollowing;
 }
 
-- (void)onEnter {
+- (void)onEnter
+{
     [super onEnter];
     self.userInteractionEnabled = TRUE;
     pathPoints = [[NSMutableArray alloc] init];
+    one = CGPointMake(50, 50);
+    two = CGPointMake(100, 50);
+    three = CGPointMake(100, 100);
+    four = CGPointMake(50, 100);
+    
+    
+    testPath = [[NSMutableArray alloc] init];
+    [testPath addObject:[NSValue valueWithCGPoint:one]];
+    [testPath addObject:[NSValue valueWithCGPoint:two]];
+    [testPath addObject:[NSValue valueWithCGPoint:three]];
+    [testPath addObject:[NSValue valueWithCGPoint:four]];
+    [testPath addObject:[NSValue valueWithCGPoint:one]];
+
+    
+    isFollowing = false;
 
 }
 
@@ -26,7 +51,9 @@
 {
     // interrupt previous movement sequence and empty the path point queue
     [self stopAllActions];
+    isFollowing = false;
     [pathPoints removeAllObjects];
+    CCLOG(@"zombie at %f  %f",self.position.x, self.position.y);
 }
 
 - (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
@@ -43,12 +70,67 @@
 
 - (void)touchEnded:(UITouch *)touch withEvent:(UIEvent *)event
 {
-    targetPoint = touchLocation;
-    id moveAction = [CCActionMoveTo actionWithDuration:2 position:targetPoint];
-    [self runAction:moveAction];
-    
+    // ** this part works
+//    targetPoint = touchLocation;
+//    id moveAction = [CCActionMoveTo actionWithDuration:2 position:targetPoint];
+//    [self runAction:moveAction];
+
+    // this part doesn't TODO
+    isFollowing = true;
+
     // tell me how big the point path queue is
-    CCLOG(@"%i",[pathPoints count]);
+    CCLOG(@"drawn path %i",[pathPoints count]);
+    
+    [self followPath:pathPoints];
+//    CCLOG(@"test path %i",[testPath count]);
+
+//    [self followPath:testPath];
+    
+//    [self followPath];
+    
 }
+
+- (void) followPath:(NSMutableArray *)points
+//- (void) followPath
+{
+    // need to create a CCActionSequence from the given array
+//    CCArray *moveSequence = CCArray::create();
+    
+//    int i = 0;
+//    CCLOG(@"yes");
+//    while (i <= [points count]) {
+//        if (i % 2 == 0) {
+//            CGPoint nextPoint = [points[i] CGPointValue];
+//            id moveAction = [CCActionMoveTo actionWithDuration:.5 position:nextPoint];
+//            [self runAction:moveAction];
+//            CCLOG(@"%i",i);
+//
+//        }
+//        i ++;
+//    }
+//    while (![moveAction isDone]) {
+//        CCLOG(@"moving!");
+//    }
+    
+    id move1 = [CCActionMoveTo actionWithDuration:1 position:one];
+    id move2 = [CCActionMoveTo actionWithDuration:1 position:two];
+    id move3 = [CCActionMoveTo actionWithDuration:1 position:three];
+    id move4 = [CCActionMoveTo actionWithDuration:1 position:four];
+    id move5 = [CCActionMoveTo actionWithDuration:1 position:one];
+    
+//    [self runAction:move1];
+//    [self runAction:move2];
+//    [self runAction:move3];
+//    [self runAction:move4];
+//    [self runAction:move5];
+    
+    [self runAction:[CCActionSequence actions: move1, move2, move3, move4, move5, nil]];
+
+//    CCActionSequence *sequence = [CCActionSequence actionWithArray:@[moveTo, mergeTile, remove]];
+
+    CCLOG(@"done");
+
+}
+
 
 @end
