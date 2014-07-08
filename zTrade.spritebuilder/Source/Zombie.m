4227ddedcd1a68400375e72d1bdf28e55b8ecc51
Jordan Arnesen <jordan101112@gmail.com>
2014-07-07 17:00:11 -0700

    creating an array of the points from touchMochMoved

--- zTrade.spritebuilder/Source/Zombie.m
+++ zTrade.spritebuilder/Source/Zombie.m
@@ -10,29 +10,45 @@
 
 @implementation Zombie {
     CCActionSequence *currentPath;
+    NSMutableArray *pathPoints;
+    CGPoint touchLocation;
     CGPoint targetPoint;
 }
 
 - (void)onEnter {
     [super onEnter];
     self.userInteractionEnabled = TRUE;
+    pathPoints = [[NSMutableArray alloc] init];
+
 }
 
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
 {
+    // interrupt previous movement sequence and empty the path point queue
+    [self stopAllActions];
+    [pathPoints removeAllObjects];
 }
 
 - (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
 {
     // we want to know the location of our touch in this scene
-    CGPoint touchLocation = [touch locationInNode:self.parent];
-    targetPoint = touchLocation;
+    touchLocation = [touch locationInNode:self.parent];
+    
+    // convert CGPoint so that it can be stored in the path point queue
+    NSValue *pointValue = [NSValue valueWithCGPoint:touchLocation];
+    
+    [pathPoints addObject:pointValue];
+    
 }
 
 - (void)touchEnded:(UITouch *)touch withEvent:(UIEvent *)event
 {
+    targetPoint = touchLocation;
     id moveAction = [CCActionMoveTo actionWithDuration:2 position:targetPoint];
     [self runAction:moveAction];
+    
+    // tell me how big the point path queue is
+    CCLOG(@"%i",[pathPoints count]);
 }
 
 @end
